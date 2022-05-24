c9e5b22dd350b42454a7d85ca189d90770942cc3
Justin Ackermann <justin.ackermann@im-c.de>
2022-05-24 08:33:35 +0200

    Done

--- youtubedownload.sh
+++ youtubedownload.sh
@@ -1,31 +1,30 @@
 #! /bin/bash
+
+echo "Welcome to CryptoJu's Youtube Playlist Downloader"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+# Checks if Youtube DL Library is installed and if not, installs it
 if [ $(dpkg-query -W -f='${Status}' youtube-dl 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-	echo "Checking if required package Youtube-dl is installed"
-	sudo -H pip install --upgrade youtube-dl
+	echo "Downloading required package youtube_dl"
+	pip install --upgrade youtube-dl
 fi
 
-echo "###############################################"
-echo "So, jetzt kanns losgehen!"
-while IFS=' ' read -r p n; do
+# Reads every line of the textfile and assigns variables for url and name
+while IFS=';' read -r p n; do
 	playlist+=( "$p" )
 	name+=( "$n" )
 done < playlists.txt
-echo "Reading playlists....."
-echo "Playlists loaded. Starting download......"
-echo "###############################################"
+
+# Downloads playlist with youtube dl and creates a folder for it
 for i in "${name[@]}"
 do
-	echo "###############################################"
 	echo "Downloading playlist $i. Creating directory...."
-	echo "###############################################"
 	mkdir -p $i
 	cd $i
 	youtube-dl --extract-audio --audio-format mp3 -i -o "%(title)s.%(ext)s" ${playlist[0]}
 	cd ..
 	playlist=("${playlist[@]:1}")
 done
-echo "############################################"
-echo "Finished downloading all specified playlists!"
-echo "Thanks for using Spastins Playlistloader. Have a nice day"
-echo "############################################"
+
+echo "Die Playlist/s wurde/n fertig heruntergeladen."
